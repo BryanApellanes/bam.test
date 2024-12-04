c3b651dd3d4e8ef568b80953ace5816a40a372f4
Bryan Apellanes <bryan.apellanes@gmail.com>
2024-12-04 09:19:47 -0600

    wip

--- bam.test/UnitTestMenu.cs
+++ bam.test/UnitTestMenu.cs
@@ -9,7 +9,7 @@ using Bam;
 namespace Bam.Test
 {
     /// <summary>
-    /// Attribute used to addorn a class that contains unit tests.
+    /// Attribute used to adorn a class that contains unit tests.
     /// </summary>
     public class UnitTestMenu : MenuAttribute<UnitTest>
     {
@@ -20,7 +20,7 @@ namespace Bam.Test
 
         public UnitTestMenu(string name) : base(name)
         {
-            this.Selector = name?.CaseAcronym().ToLowerInvariant() ?? "ut";
+            this.Selector = name?.PascalCase(true, " ").CaseAcronym().ToLowerInvariant() ?? "ut";
         }
 
         public UnitTestMenu(string name, string selector) : base(name)
--- bam.test/UnitTestMenuContainer.cs
+++ bam.test/UnitTestMenuContainer.cs
@@ -6,12 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bam.Incubation;
 
 namespace Bam.Test
 {
     [UnitTestMenu()]
     public abstract class UnitTestMenuContainer : MenuContainer
     {
+        public UnitTestMenuContainer() : this(BamConsoleContext.GetDefaultServiceRegistry())
+        {
+        }
+
         /// <summary>
         /// Create a new UnitTestMenuContainer using the specified dependency provider.
         /// </summary>
@@ -19,16 +24,35 @@ namespace Bam.Test
         public UnitTestMenuContainer(ServiceRegistry serviceRegistry)
             : base() 
         {
-            this.SetDependencyProvider(this.Configure(serviceRegistry));
+            this.SetDependencyProvider(serviceRegistry);
         }
 
+        public ServiceRegistry Configure(ServiceRegistry serviceRegistry)
+        {
+            if (DependencyProvider != null)
+            {
+                serviceRegistry.CopyFrom((DependencyProvider)DependencyProvider);
+            }
+
+            DependencyProvider = serviceRegistry;
+            return serviceRegistry;
+        }
         /// <summary>
         /// Configures the specified service registry before setting as the DependencyProvider property.
         /// </summary>
         /// <param name="serviceRegistry">The service registry.</param>
         /// <returns>ServiceRegistry</returns>
-        public virtual ServiceRegistry Configure(ServiceRegistry serviceRegistry) 
+        public ServiceRegistry Configure(Action<ServiceRegistry>? configure = null)
         {
+            configure = configure ?? ((svc) => { });
+            ServiceRegistry serviceRegistry = new ServiceRegistry();
+            if (DependencyProvider != null)
+            {
+                serviceRegistry.CopyFrom((DependencyProvider)DependencyProvider);
+            }
+
+            configure(serviceRegistry);
+            DependencyProvider = serviceRegistry;
             return serviceRegistry;
         }
     }
